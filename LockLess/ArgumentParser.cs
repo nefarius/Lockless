77b48209a600862ba8397cc254368b75b0543134
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-08-12 21:32:19 +0200

    Ran code clean-up and formatting rules

--- LockLess/ArgumentParser.cs
+++ LockLess/ArgumentParser.cs
@@ -1,31 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace LockLess
-{
-    public static class ArgumentParser
-    {
-        public static ArgumentParserResult Parse(IEnumerable<string> args)
-        {
-            var arguments = new Dictionary<string, string>();
-            try
-            {
-                foreach (var argument in args)
-                {
-                    var idx = argument.IndexOf(':');
-                    if (idx > 0)
-                        arguments[argument.Substring(0, idx)] = argument.Substring(idx + 1);
-                    else
-                        arguments[argument] = string.Empty;
-                }
+namespace LockLess;
 
-                return ArgumentParserResult.Success(arguments);
-            }
-            catch (System.Exception ex)
+public static class ArgumentParser
+{
+    public static ArgumentParserResult Parse(IEnumerable<string> args)
+    {
+        var arguments = new Dictionary<string, string>();
+        try
+        {
+            foreach (var argument in args)
             {
-                Debug.WriteLine(ex.Message);
-                return ArgumentParserResult.Failure();
+                var idx = argument.IndexOf(':');
+                if (idx > 0)
+                    arguments[argument.Substring(0, idx)] = argument.Substring(idx + 1);
+                else
+                    arguments[argument] = string.Empty;
             }
+
+            return ArgumentParserResult.Success(arguments);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return ArgumentParserResult.Failure();
         }
     }
 }
--- LockLess/ArgumentParserResult.cs
+++ LockLess/ArgumentParserResult.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections.Generic;
 
-namespace LockLess
+namespace LockLess;
+
+public class ArgumentParserResult
 {
-    public class ArgumentParserResult
+    private ArgumentParserResult(bool parsedOk, Dictionary<string, string> arguments)
     {
-        public bool ParsedOk { get; }
-        public Dictionary<string, string> Arguments { get; }
+        ParsedOk = parsedOk;
+        Arguments = arguments;
+    }
 
-        private ArgumentParserResult(bool parsedOk, Dictionary<string, string> arguments)
-        {
-            ParsedOk = parsedOk;
-            Arguments = arguments;
-        }
+    public bool ParsedOk { get; }
+    public Dictionary<string, string> Arguments { get; }
 
-        public static ArgumentParserResult Success(Dictionary<string, string> arguments)
-            => new ArgumentParserResult(true, arguments);
+    public static ArgumentParserResult Success(Dictionary<string, string> arguments)
+    {
+        return new ArgumentParserResult(true, arguments);
+    }
 
-        public static ArgumentParserResult Failure()
-            => new ArgumentParserResult(false, null);
-
+    public static ArgumentParserResult Failure()
+    {
+        return new ArgumentParserResult(false, null);
     }
 }
